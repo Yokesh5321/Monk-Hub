b1cae91204e698910b606247cd57ac80aa1ed48b
Yokesh5321 <310625243254@eec.srmrmp.edu.in>
2026-05-22 14:47:38 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,16 @@
+var menu1=document.querySelector(".h2")
+var menu=document.querySelector(".menu")
+function button(){
+    menu.style.display="flex"
+}
+function back(){
+    menu.style.display="none"
+}
+var login=document.getElementById("center")
+var bagi=document.getElementById("bagi")
+function monk(){
+    login.style.display="block"
+}
+function bagin(){
+    login.style.display="none"
+}
